665105fc9b9505992a3e707363158e02cfe2d02a
Stephen Adams <tsadams@gmail.com>
2019-02-19 13:39:53 -0500

    Woops

--- utils/notifications.py
+++ utils/notifications.py
@@ -118,8 +118,8 @@ class SubscriptionConfirmation(BaseNotification):
         """
         h = httplib2.Http()
         response, content = h.request(self.raw_message['SubscribeURL'], 'GET')
+        self.message = "Subscribed"
         # Todo:: log it app.logger.debug("topic subscription response: [%s]\n%s" % (response.status, content))
-        self.message('Subscribed')
         return response, content
 
 
